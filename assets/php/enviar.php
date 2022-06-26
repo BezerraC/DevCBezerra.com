dec266f4b2ca7926e2b05b264a87ac279d137b13
Carlos Bezerra <cbezerraneto@gmail.com>
2022-06-26 20:46:15 -0300

    New PHP send email method

--- assets/php/enviar.php
+++ assets/php/enviar.php
@@ -13,7 +13,7 @@ HTML;
 
 $headers = "MIME-Version: 1.0 \r\n";
 $headers.= "Content-type: text/html; charset=utf-8 \r\n";
-$headers.= "From: $nome <$email> \r\n";
+$headers.= "From: Devcbezerra | $nome <$email> \r\n";
 $headers.= "To: devcbezerra.com \r\n";
 // $headers.= "Cc: cbezerraneto@gmail.com \r\n";
 // $headers.= "Bcc: cbezerraneto@gmail.com \r\n";
@@ -29,7 +29,7 @@ $rta = mail('cbezerraneto@gmail.com',
 if (mail($subject, $body, $headers)){
   echo ("<script>
         alert('Email enviado com sucesso');
-        window.history.back();
+        document.location.href='../../'
     </script>");
 }else {
   echo ("<script>
