6138c321949ad5b67aff47bb14c2b983969819fc
Carlos Bezerra <cbezerraneto@gmail.com>
2022-06-26 20:27:04 -0300

    New contact method

--- assets/php/enviar.php
+++ assets/php/enviar.php
@@ -5,25 +5,26 @@ $subject = $_GET['subject'];
 $mensagem = $_GET['mensagem'];
 
 $body = <<<HTML
-    <h1>Contado DevCBezerra.com</h1>
-    <p>De: $nome / $email</p><br/>
+    <h2>Contato | DevCBezerra</h2>
+    <h5>De: $nome / $email</h5>
     <h2>Mensagem</h2>
-    <p>$mensagem</p>
+    <h5>$mensagem</h5>
 HTML;
 
 $headers = "MIME-Version: 1.0 \r\n";
-$headers.= "Content-type: text/html; 
-charset=utf-8 \r\n";
+$headers.= "Content-type: text/html; charset=utf-8 \r\n";
 $headers.= "From: $nome <$email> \r\n";
-$headers.= "To: cbezerraneto@gmail.com \r\n";
+$headers.= "To: devcbezerra.com \r\n";
 // $headers.= "Cc: cbezerraneto@gmail.com \r\n";
 // $headers.= "Bcc: cbezerraneto@gmail.com \r\n";
 
 //configuracao da mensagem
-var_dump($nome);
+// var_dump($nome);
 $rta = mail('cbezerraneto@gmail.com',
 "Assunto: $subject", $body, $headers );
-var_dump($rta);
+// var_dump($rta);
+
+header("Location: thanks.html");
 
 // if ($rta->send()){
 //   echo ("<script>
