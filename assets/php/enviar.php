64302348301c88ee49cfbf5f1f16f091b066e973
Carlos Bezerra <cbezerraneto@gmail.com>
2022-06-26 19:10:01 -0300

    New contact method

--- assets/php/enviar.php
+++ assets/php/enviar.php
@@ -1,51 +1,29 @@
 <?php  
-header("Set-Cookie: key4=value; path=/; domain=www.devcbezerra.com; HttpOnly; Secure; SameSite=Strict");
+if($_SERVER['REQUEST_METHOD'] != 'POST'){
+  header("Location: Index.html");
+}
 
-$nome = utf8_encode($_GET['nome']);
-$email = utf8_encode($_GET['email']);
-$projeto = utf8_encode($_GET['projeto']);
-$mensagem = utf8_encode($_GET['mensagem']);
-
-require 'PHPMailer/PHPMailerAutoload.php';
-
-$mail = new PHPMailer;
-$mail->isSMTP();
-
-//Configuracoes do servidor de email
-
-$mail->Host = "smtp.hostinger.com";
-$mail->Port = "465";
-$mail->SMTPSecure = "ssl";
-$mail->SMTPAuth = "true";
-$mail->Username = "carlos@devcbezerra.com";
-$mail->Password = "Sbwxdr2d2x@";
+$nome = $_GET['nome'];
+$email = $_GET['email'];
+$subject = $_GET['subject'];
+$mensagem = $_GET['mensagem'];
 
 //configuracao da mensagem
+var_dump($nome);
+$rta = mail('carlos@devcbezerra.com',
+"Mensagem: $subject", $mensagem );
+var_dump($rta);
 
-$mail->setFrom($mail->Username, "DevCBezerra"); // Remetente
-$mail->addAddress('cbezerraneto@gmail.com'); // Destinatario
-$mail->Subject = "DevCBezerra"; // Assunto e-mail
-
-$conteudo_email = "
-Você recebeu uma mensagem de: $nome E-mail: $email
-<br><br>
-Projeto: $projeto  <br><br>
-Mensagem: <br> 
-$mensagem
-";
-
-$mail->IsHTML(true);
-$mail->Body = $conteudo_email;
-
-if ($mail->send()){
-  echo ("<script>
-        alert('Email enviado com sucesso');
-        window.history.back();
-    </script>");
-}else {
-  echo ("<script>
-        window.alert('Falha ao enviar o Email')
-        window.history.back();
-    </script>");
-}
+// if ($mail->send()){
+//   echo ("<script>
+//         alert('Email enviado com sucesso');
+//         window.history.back();
+//     </script>");
+// }else {
+//   echo ("<script>
+//         window.alert('Falha ao enviar o Email')
+//         window.history.back();
+//     </script>");
+// }
+// 
 ?>
