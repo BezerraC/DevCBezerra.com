ee7352cad40d9e85ca77427a0d4e222e70111fff
Carlos Bezerra <cbezerraneto@gmail.com>
2022-06-26 20:11:49 -0300

    New contact method

--- assets/php/enviar.php
+++ assets/php/enviar.php
@@ -1,27 +1,32 @@
 <?php  
-
-
 $nome = $_GET['nome'];
 $email = $_GET['email'];
 $subject = $_GET['subject'];
 $mensagem = $_GET['mensagem'];
 
-//configuracao da mensagem
-// var_dump($nome);
-$rta = mail('cbezerraneto@gmail.com',
-"Assunto: $subject", $mensagem );
-// var_dump($rta);
+$body = <<<HTML
+    <h1>Contado DevCBezerra.com</h1>
+    <p>De: $nome / $email</p><br/>
+    <h2>Mensagem</h2>
+    <p>$mensagem</p>
+HTML;
 
-if ($rta->send()){
-  echo ("<script>
-        alert('Email enviado com sucesso');
-        window.history.back();
-    </script>");
-}else {
-  echo ("<script>
-        window.alert('Falha ao enviar o Email')
-        window.history.back();
-    </script>");
-}
+//configuracao da mensagem
+var_dump($nome);
+$rta = mail('cbezerraneto@gmail.com',
+"Assunto: $subject", $body );
+var_dump($rta);
+
+// if ($rta->send()){
+//   echo ("<script>
+//         alert('Email enviado com sucesso');
+//         window.history.back();
+//     </script>");
+// }else {
+//   echo ("<script>
+//         window.alert('Falha ao enviar o Email')
+//         window.history.back();
+//     </script>");
+// }
 
 ?>
